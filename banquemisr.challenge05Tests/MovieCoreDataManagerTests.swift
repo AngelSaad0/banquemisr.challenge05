1bb3ebaa776f378b4dcc1b12a91a6401115925f4
AngelSaad0 <engysaad573new@gmail.com>
2024-10-06 20:45:23 +0300

    Added NetworkManager and MovieCoreDataManager Tests

---
+++ banquemisr.challenge05Tests/MovieCoreDataManagerTests.swift
@@ -0,0 +1,93 @@
+//
+//  MovieCoreDataManagerTests.swift
+//  banquemisr.challenge05Tests
+//
+//  Created by Engy on 06/10/2024.
+//
+
+import XCTest
+@testable import banquemisr_challenge05
+
+final class MovieCoreDataManagerTests: XCTestCase {
+
+    var coreDataManager: MovieCoreDataServiceProtocol!
+    
+    override func setUpWithError() throws {
+        coreDataManager = MovieCoreDataManager.shared
+        clearEntityCache()
+    }
+
+    override func tearDownWithError() throws {
+        coreDataManager = nil
+//        clearEntityCache()
+    }
+
+    func clearEntityCache() {
+        if let appDelegate = UIApplication.shared.delegate as? AppDelegate {
+            let context = appDelegate.persistentContainer.viewContext
+            clearCoreData(for: "MovieEntity", context: context)
+        }
+    }
+    
+    func testSavingAndGettingMoviesFromCoreData() throws {
+        var (movies, images) = coreDataManager.getMovies(category: .nowPlaying)
+        XCTAssertEqual(movies.count, 0)
+        
+        coreDataManager.storeMovies(movies: [
+            Movie(revenue: nil, budget: nil, adult: false, backdropPath: "/gZWl93sf8AxavYpVT1Un6EF3oCj.jpg", id: 475557, originalLanguage: "en", overview: "", popularity: 903.781, posterPath: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", releaseDate: "2019-10-01", title: "Joker", voteAverage: 8.154, voteCount: 25301, genres: nil, runtime: nil, tagline: nil)
+        ], category: .nowPlaying)
+        
+        (movies, images) = coreDataManager.getMovies(category: .nowPlaying)
+        XCTAssertEqual(movies.count, 1)
+        XCTAssertEqual(movies.count, images.count)
+        XCTAssertEqual(movies.first?.id, 475557)
+    }
+
+    func testGettingMovieFromCoreData() throws {
+        coreDataManager.storeMovies(movies: [
+            Movie(revenue: nil, budget: nil, adult: false, backdropPath: "/gZWl93sf8AxavYpVT1Un6EF3oCj.jpg", id: 475557, originalLanguage: "en", overview: "", popularity: 903.781, posterPath: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", releaseDate: "2019-10-01", title: "Joker", voteAverage: 8.154, voteCount: 25301, genres: nil, runtime: nil, tagline: nil)
+        ], category: .nowPlaying)
+        
+        let (movie, image) = coreDataManager.getMovie(forMovieWithId: 475557)
+        XCTAssertEqual(movie?.id, 475557)
+        XCTAssertEqual(movie?.title, "Joker")
+        XCTAssertNil(image)
+    }
+    
+    func testUpdateMovieFromCoreData() throws {
+        coreDataManager.storeMovies(movies: [
+            Movie(revenue: nil, budget: nil, adult: false, backdropPath: "/gZWl93sf8AxavYpVT1Un6EF3oCj.jpg", id: 475557, originalLanguage: "en", overview: "", popularity: 903.781, posterPath: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", releaseDate: "2019-10-01", title: "Joker", voteAverage: 8.154, voteCount: 25301, genres: nil, runtime: nil, tagline: nil)
+        ], category: .nowPlaying)
+        
+        var (movie, _) = coreDataManager.getMovie(forMovieWithId: 475557)
+        XCTAssertEqual(movie?.revenue, 0)
+        XCTAssertEqual(movie?.budget, 0)
+        XCTAssertNil(movie?.genres?.first)
+        XCTAssertEqual(movie?.runtime, 0)
+        XCTAssertNil(movie?.tagline)
+        
+        let updatedMovie = Movie(revenue: 500_000, budget: 1_000_000, adult: false, backdropPath: "/gZWl93sf8AxavYpVT1Un6EF3oCj.jpg", id: 475557, originalLanguage: "en", overview: "", popularity: 903.781, posterPath: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", releaseDate: "2019-10-01", title: "Joker", voteAverage: 8.154, voteCount: 25301, genres: [Genre(id: 1, name: "Horror")], runtime: 93, tagline: "Updated Tagline")
+        
+        coreDataManager.updateMovie(withId: 475557, updatedMovie: updatedMovie)
+        
+        (movie, _) = coreDataManager.getMovie(forMovieWithId: 475557)
+        XCTAssertEqual(movie?.revenue, 500_000)
+        XCTAssertEqual(movie?.budget, 1_000_000)
+//        XCTAssertEqual(movie?.genres?.first?.id, 1)
+//        XCTAssertEqual(movie?.genres?.first?.name, "Horror")
+        XCTAssertEqual(movie?.runtime, 93)
+        XCTAssertEqual(movie?.tagline, "Updated Tagline")
+    }
+
+    func testStoreMovieImage() throws {
+        coreDataManager.storeMovies(movies: [
+            Movie(revenue: nil, budget: nil, adult: false, backdropPath: "/gZWl93sf8AxavYpVT1Un6EF3oCj.jpg", id: 475557, originalLanguage: "en", overview: "", popularity: 903.781, posterPath: "/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg", releaseDate: "2019-10-01", title: "Joker", voteAverage: 8.154, voteCount: 25301, genres: nil, runtime: nil, tagline: nil)
+        ], category: .nowPlaying)
+        
+        let testingData = "Testing".data(using: .utf8) ?? Data()
+        coreDataManager.storeMovieImage(testingData, forMovieWithId: 475557, imageType: .backdrop)
+        
+        let (_, image) = coreDataManager.getMovie(forMovieWithId: 475557)
+        XCTAssertEqual(image, testingData)
+    }
+}
--- banquemisr.challenge05Tests/NetworkManagerTests.swift
+++ banquemisr.challenge05Tests/NetworkManagerTests.swift
@@ -20,12 +20,70 @@ final class NetworkManagerTests: XCTestCase {
         networkManager = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testFetchingUpcomingMoviesFromAPI() throws {
+        let expectation = expectation(description: "Testing successful fetching upcoming movies from API")
+        networkManager.fetchData(from: MovieAPIProvider.upcoming, responseType: Movies.self) { result, error in
+            if error != nil {
+                XCTFail()
+            }
+            guard let result = result else {
+                XCTFail()
+                return
+            }
+            XCTAssertGreaterThan(result.results.count, 0)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+    }
+    
+    func testFetchingMovieDetailsFromAPI() throws {
+        let expectation = expectation(description: "Testing successful fetching movie details from API")
+        networkManager.fetchData(from: MovieAPIProvider.details(id: 475557), responseType: Movie.self) { result, error in
+            if error != nil {
+                XCTFail()
+            }
+            guard let result = result else {
+                XCTFail()
+                return
+            }
+            XCTAssertEqual(result.id, 475557)
+            XCTAssertEqual(result.title, "Joker")
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+    }
+    
+    func testNetworkDecodingFailure() throws {
+        let expectation = expectation(description: "Testing failure decoding movies from API because of wrong response type")
+        networkManager.fetchData(from: MovieAPIProvider.upcoming, responseType: Movie.self) { result, error in
+            if result != nil {
+                XCTFail()
+            }
+            guard let error = error else {
+                XCTFail()
+                return
+            }
+            XCTAssertEqual(error.errorDescription, APIError.decodingFailure.errorDescription)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5)
     }
 
+    func testLoadImageFromAPI() throws {
+        let expectation = expectation(description: "Testing successful loading image from API")
+        networkManager.loadImage(from: "/gZWl93sf8AxavYpVT1Un6EF3oCj.jpg") { data in
+            XCTAssertNotNil(data)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+    }
+    
+    func testFailureLoadImageFromAPI() throws {
+        let expectation = expectation(description: "Testing failure loading image from API because of invalid image path")
+        networkManager.loadImage(from: "invalidPath") { data in
+            XCTAssertNil(data)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5)
+    }
 }
